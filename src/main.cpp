6d890cbd9107da7041ff9084ab4ef52c9d8c2579
David Eldridge <david.eldridge1987@gmail.com>
2024-10-17 14:33:38 +1030

    WiFi Manager Added in WiFi Manager Captive Portal to enable own connection information

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,46 @@
 #include <Arduino.h>
 #include <WebServer.h>
 #include <LEDController.h>
+#include <WiFiManager.h>
+
+// WiFI Manager
+WiFiManager wm;
 
 // put function declarations here:
 bool animationRunning = false;
 
 void setup() {
+  Serial.begin(115200);
+  
+  // Set Up WiFi
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP        
+  wm.setHostname("Inifnity Mirror");
+  // reset settings - wipe credentials for testing
+  // wm.resetSettings();
+
+  wm.setConfigPortalBlocking(false);
+  wm.setConfigPortalTimeout(60);
+  
+  // Automatically connect using saved credentials if they exist
+  // If connection fails it starts an access point with the specified name
+  if(wm.autoConnect("Unconnected Infinity Mirror")) {
+    Serial.println("connected...yeey :)");
+  }
+  else {
+    Serial.println("Configportal running");
+  }
+
+
+  // Initalise the LED Strip and set the default colour to Red
   initStrip();
   changeCylonColour(HtmlColor(0x7f0000));
 }
 
 void loop() {
-    animationSelector(6);
-    animationRunning = true;
+  // Process the WiFi Manager Captive Portal
+  wm.process();
+  
+  
+  animationSelector(6);
+  animationRunning = true;
 }
